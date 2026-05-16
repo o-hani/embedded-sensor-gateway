6849ecc598a0c33336b4ef74852925d93c5f999e
Okba HANI <oqba.hani@gmail.com>
2026-05-16 19:40:42 +0200

    Jour 2: gestion mémoire avec unique_ptr

---
+++ semaine1/jour2/capteur.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <memory> 
+
+// Création calsse Capteur
+class Capteur {
+    public:
+        Capteur(const std::string& nom)
+            : m_nom(nom)
+            , m_valeur(0.0)
+        {
+            std::cout << "Capteur " << m_nom << " créé\n";
+        }
+
+        virtual ~Capteur() {
+            std::cout << "Capteur " << m_nom << " détruit\n";
+        }
+
+        virtual void lireValeur() = 0;
+
+        double getValeur() const {
+            return m_valeur;
+        }
+
+        std::string getId() const {
+            return m_nom;
+        }
+
+    protected:
+        std::string m_nom;
+        double m_valeur;
+};
+
+// Création calsse fille CapteurTemperature
+class CapteurTemperature : public Capteur {
+    public:
+        CapteurTemperature(const std::string& nom): Capteur(nom) {}
+        
+        void lireValeur() override {
+            m_valeur = 36.6;
+        }
+
+        double enFahrenheit() const {
+            return (m_valeur * 9/5) + 32;
+        }
+};
+
+int main() {
+    std::unique_ptr<Capteur> c = std::make_unique<CapteurTemperature>("température");
+    c->lireValeur();
+    std::cout << c->getValeur() << "\n";
+    //std::cout << c->enFahrenheit() << "\n";
+    return 0;
+}
